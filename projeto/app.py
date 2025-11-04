6b1f9d168b804372bf4df47353918313bd451764
Luanhenrique12 <hluan8867@gmail.com>
2025-11-04 15:40:04 -0300

    Top 3 filminhos

--- projeto/app.py
+++ projeto/app.py
@@ -3,10 +3,8 @@ from flask import Flask, jsonify, render_template
 app = Flask(__name__)
 filminhos = { '1': {'id': '1', 'titulo': 'One piece'}, 
             '2': { 'id': '2', 'titulo': 'Motoqueiro Fantasma '},
-            '3': { 'id': '3', 'titulo': 'Scarface', 'diretor': 'Brian De Palma', 'genero': 'Crime / Drama',
-                 'poster': '/static/posteres/scarface-2.jpg', 'ano': 1983 }, 
-            '4': { 'id': '4', 'titulo': 'Os Bons Companheiros', 'diretor': 'Martin Scorsese', 'genero': 'Crime / Biografia / Drama',
-                 'poster': '/static/posteres/l_54529_0099685_307a5bd2.jpg', 'ano': 1990 } 
+            '3': { 'id': '3', 'titulo': 'Invocação do mal' }, 
+            
         }
 
 
